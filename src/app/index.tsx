719fe7be0ceac3d19b18b7415179c11c12492ed4
sakarkhadka10 <khadka.sakar10@gmail.com>
2026-03-23 23:52:36 +0545

    chore: update dependencies and add Tailwind CSS support
    - Added babel-preset-expo and nativewind for styling.
    - Updated react-native-reanimated to use caret version.
    - Introduced global CSS file for Tailwind directives.
    - Configured Tailwind CSS with a new tailwind.config.js file.
    - Modified _layout.tsx to import global styles.
    - Enhanced index.tsx to utilize Tailwind CSS classes for styling.
    - Updated tsconfig.json to include nativewind-env.d.ts for type definitions.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500 text-2xl bg-black p-6 rounded-2xl mb-6 ">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
       <Image
         source={require("@/assets/images/icon.png")}
         style={{ width: 200, height: 200, borderRadius: 20 }}
